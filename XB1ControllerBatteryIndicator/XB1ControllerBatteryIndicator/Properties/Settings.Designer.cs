dc3271da9c2d52cb2c86eab474f3690b3ddca52b
Friday-The13-rus <rogerup.rzn@gmail.com>
2020-11-24 21:21:04 +0300

    Fixed settings

--- XB1ControllerBatteryIndicator/XB1ControllerBatteryIndicator/Properties/Settings.Designer.cs
+++ XB1ControllerBatteryIndicator/XB1ControllerBatteryIndicator/Properties/Settings.Designer.cs
@@ -98,42 +98,6 @@ namespace XB1ControllerBatteryIndicator.Properties {
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool ShowToastOnLowBattery {
-            get {
-                return ((bool)(this["ShowToastOnLowBattery"]));
-            }
-            set {
-                this["ShowToastOnLowBattery"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("False")]
-        public bool ShowPopupOnLowBattery {
-            get {
-                return ((bool)(this["ShowPopupOnLowBattery"]));
-            }
-            set {
-                this["ShowPopupOnLowBattery"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
-        public bool ShowPopupOnGuide {
-            get {
-                return ((bool)(this["ShowPopupOnGuide"]));
-            }
-            set {
-                this["ShowPopupOnGuide"] = value;
-            }
-        }
-        
-        [global::System.Configuration.UserScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("True")]
         public bool LowBatteryToast_Enabled {
             get {
                 return ((bool)(this["LowBatteryToast_Enabled"]));
@@ -169,25 +133,26 @@ namespace XB1ControllerBatteryIndicator.Properties {
         
         [global::System.Configuration.UserScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute(@"<?xml version=""1.0"" encoding=""utf-16""?>
-<SimpleBatteryPopupSettings xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
-  <Size>
-    <Width>300</Width>
-    <Height>60</Height>
-  </Size>
-  <X>0.5</X>
-  <AlignmentX>Left</AlignmentX>
-  <Y>0.8</Y>
-  <AlignmentY>Top</AlignmentY>
-  <DisplayDuration>0:00:03</DisplayDuration>
-  <BackgroundColor>#FFA9A9A9</BackgroundColor>
-  <ForegroundColor>#FFF8F8FF</ForegroundColor>
-  <FontSize>20</FontSize>
-  <FontName>Segoe UI</FontName>
-  <FontBold>false</FontBold>
-  <FontItalic>false</FontItalic>
-  <FontUnderline>false</FontUnderline>
-</SimpleBatteryPopupSettings>")]
+        [global::System.Configuration.DefaultSettingValueAttribute(@"
+          <SimpleBatteryPopupSettings xmlns:xsi=""http://www.w3.org/2001/XMLSchema-instance"" xmlns:xsd=""http://www.w3.org/2001/XMLSchema"">
+            <Size>
+              <Width>300</Width>
+              <Height>60</Height>
+            </Size>
+            <X>0.5</X>
+            <AlignmentX>Left</AlignmentX>
+            <Y>0.8</Y>
+            <AlignmentY>Top</AlignmentY>
+            <DisplayDuration>0:00:03</DisplayDuration>
+            <BackgroundColor>#FFA9A9A9</BackgroundColor>
+            <ForegroundColor>#FFF8F8FF</ForegroundColor>
+            <FontSize>20</FontSize>
+            <FontName>Segoe UI</FontName>
+            <FontBold>false</FontBold>
+            <FontItalic>false</FontItalic>
+            <FontUnderline>false</FontUnderline>
+          </SimpleBatteryPopupSettings>
+        ")]
         public global::XB1ControllerBatteryIndicator.BatteryPopup.SimpleBatteryPopupSettings PopupSettings {
             get {
                 return ((global::XB1ControllerBatteryIndicator.BatteryPopup.SimpleBatteryPopupSettings)(this["PopupSettings"]));
